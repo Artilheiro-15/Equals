a219ee74c47e614302411afb1132056d1b775c26
Atila jr <atilla.junior33@gmail.com>
2023-01-06 16:28:57 -0300

    Método que compara se o objeto é igual a outro, retornando true ou false

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+
+  public static void main(String[] args) throws Exception {
+    //Equals Método que compara se o objeto é igual a outro, retornando true ou false.
+    System.out.println("==================================");
+
+    String a = "Maria";
+    String b = "Alex";
+    System.out.println(a.equals(b));
+    System.out.println("==================================");
+  }
+}
